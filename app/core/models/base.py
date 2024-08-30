847630bccaf6d297c8fecd83ac02a569254b6f50
Timofey <nsk.timka2077@gmail.com>
2024-08-30 21:35:50 +0200

    add convention for unique constraint

--- app/core/models/base.py
+++ app/core/models/base.py
@@ -1,8 +1,17 @@
+from logging import config
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, declared_attr, mapped_column, Mapped
+
+from ..config import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention
+    )
+
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
